cb6fa1753232d297fc7321778fb017655aa2caf1
Abin vk <vettikkadanabin@gmail.com>
2026-01-03 15:28:01 +0530

    study

---
+++ Study Projects/project 3/script.js
@@ -0,0 +1,13 @@
+let count = 0;
+
+const btnJs = 
+document.getElementById('btnJs');
+
+const countDsplay = 
+document.getElementById('count');
+
+btnJs.addEventListener("click",() =>{
+    count++;
+    countDsplay.textContent = count;
+
+});
