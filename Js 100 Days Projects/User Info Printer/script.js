4ea905451623558fcacb4b8e34730f791bd0aaaf
Abin vk <vettikkadanabin@gmail.com>
2026-01-08 21:07:21 +0530

    challenge

---
+++ Js 100 Days Projects/User Info Printer/script.js
@@ -0,0 +1,10 @@
+let  name = "abin";    // string
+let age = 23;          // number
+let isStudent = true;  // boolean
+
+console.log(
+    "Name: " + name +
+    ", Age: " + age +
+    ", Student: " + isStudent
+)
+
---
+++ Js 100 Days Projects/User Info Printer/user.js
@@ -0,0 +1,4 @@
+export const user = {
+    Name = 'abin',
+    age = 23
+};
