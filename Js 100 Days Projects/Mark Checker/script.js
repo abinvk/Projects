9ddba1df0775f7c2460513b725ab51364b8dbe3f
Abin vk <vettikkadanabin@gmail.com>
2026-01-09 09:49:48 +0530

    challenge

--- Js 100 Days Projects/Mark Checker/script.js
+++ Js 100 Days Projects/Mark Checker/script.js
@@ -2,7 +2,7 @@ let mark = 45;
 
 if(mark >=0 && mark <=20){
     console.log("fail");
-}else if(mark <= 48){
+}else if(mark <= 68){
     console.log("pass");
 }else{
     console.log("aa");
---
+++ Js 100 Days Projects/Traffic Light System/script.js
@@ -0,0 +1,11 @@
+let light = "red";
+
+if (light === "red"){
+    console.log("stop");
+}else if (light === "green"){
+    console.log("go");
+}else if (light === "yellow"){
+    console.log("caution");
+}else{
+    console.log("invalid light color");
+}
