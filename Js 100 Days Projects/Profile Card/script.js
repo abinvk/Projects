1d76531f4af0558c593edf1dffedbbfc11d59cdf
Abin vk <vettikkadanabin@gmail.com>
2026-01-09 20:42:16 +0530

    challenge

---
+++ Js 100 Days Projects/Profile Card/script.js
@@ -0,0 +1,10 @@
+ let profile = {
+    name: "John Doe", 
+    
+    age: 30, 
+    
+    occupation: "Software Developer",}
+
+console.log("Name: " + profile.name);
+console.log("Age: " + profile.age);
+console.log("Occupation: " + profile.occupation);
