8d36577f7148d53f091f5f4ee4f4a7f36dc00300
Abin vk <vettikkadanabin@gmail.com>
2026-01-09 17:08:15 +0530

    challenge

---
+++ Js 100 Days Projects/Shopping List Manager/script.js
@@ -0,0 +1,7 @@
+let products= ["Milk", "Bread", "Eggs"];
+products.push("butter");
+console.log(products);
+products.pop();
+console.log(products);
+products.splice(2, 0, "Bananas");
+console.log(products);
