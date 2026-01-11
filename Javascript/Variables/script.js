c7a6a35f293bf8a3bdb075ccb0ff461c26fbfb14
Abin vk <vettikkadanabin@gmail.com>
2026-01-11 11:29:11 +0530

    jaavscript

--- Javascript/Variables/script.js
+++ Javascript/Variables/script.js
@@ -26,7 +26,43 @@ console.log(ab);
 let isActive = true;
 console.log(isActive);
 
-  //11. Store null in a variable.
-  
+//11. Store null in a variable.
+
 let data = null;
 console.log(data);
+
+//Store undefined explicitly in a variable.
+
+let result = undefined;
+console.log(result);
+
+//Store a floating-point number in a variable.
+
+let x = 0.2;
+console.log(x);
+
+//14. Store a negative number in a variable
+
+let y = -3;
+console.log(y);
+
+//Print a variable value to the console.
+
+let firstName = "John";
+console.log(firstName);
+
+//16. Declare a variable using var.
+
+var city = "New York";
+console.log(city);
+
+//17. Declare a variable using let
+
+let abin = "Vw";
+console.log(abin);
+
+//18. Declare a variable using const.
+
+const aby = "abin";
+console.log(aby);
+
