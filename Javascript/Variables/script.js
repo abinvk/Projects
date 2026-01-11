7526fd1a4229dc4536cb07b88c63a3a445da17f7
Abin vk <vettikkadanabin@gmail.com>
2026-01-11 10:43:06 +0530

    javascript

---
+++ Javascript/Variables/script.js
@@ -0,0 +1,14 @@
+let userName = "Abin";
+console.log(userName);
+
+let age = 25;
+console.log(age);
+
+let isStudent ;
+console.log(isStudent);
+
+let score ;
+console.log(score);
+
+let a = 10, b = 20, c = 30, d  = 40;
+console.log(a, b, c, d);
