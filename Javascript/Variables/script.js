3ebc59953290de591a0428670d702fab3657cbe1
Abin vk <vettikkadanabin@gmail.com>
2026-01-11 10:54:57 +0530

    javascript

--- Javascript/Variables/script.js
+++ Javascript/Variables/script.js
@@ -12,3 +12,21 @@ console.log(score);
 
 let a = 10, b = 20, c = 30, d  = 40;
 console.log(a, b, c, d);
+
+let qas = 12345;
+console.log(qas);
+
+//9. Store a string in a variable.
+
+let ab = "Hello, World!";
+console.log(ab);
+
+//10. Store a boolean value in a variable.
+
+let isActive = true;
+console.log(isActive);
+
+  //11. Store null in a variable.
+  
+let data = null;
+console.log(data);
