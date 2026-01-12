24b1f66f65e7fbc24903727de33407cc28e1e549
Abin vk <vettikkadanabin@gmail.com>
2026-01-12 19:45:24 +0530

    javascript

---
+++ Javascript/Variables Practice/script.js
@@ -0,0 +1,89 @@
+// 1. Store two numbers and calculate sum
+
+let num1 = 20;
+let num2 = 30;
+
+let sum = num1 + num2;
+console.log("Sum:", sum);
+
+// 2. Store three numbers and calculate total
+
+let a =10;
+let b = 5;
+let c = 20;
+
+let result = a + b + c;
+console.log("Result:", result);
+
+//3. Store marks and calculate total marks
+
+let english = 85;
+let math = 90;
+let science = 78;
+let totalMarks = english + math + science;
+console.log("Total Marks:", totalMarks);
+
+//4. Store marks and calculate average
+ 
+let malayalam = 88;
+let social = 92;
+let it = 95;
+
+let totalsMarks =  malayalam + social + it;
+console.log("New Total Marks:", totalsMarks);
+let average = totalsMarks / 3;
+console.log("Average Marks:", average);
+
+//5. Store length & width and calculate rectangle area
+
+let  length = 15;
+let  width = 10;
+
+let area = length * width;
+console.log("Area of Rectangle:", area);    
+
+//6. Store radius and calculate circle area
+
+let radius = 7;
+let circleArea = 3.14 * radius * radius;
+console.log("Area of Circle:", circleArea);
+
+
+//7. Store base & height and calculate triangle area
+
+let base = 12;
+let height = 8;
+let triangleArea = ( base * height) / 2;
+console.log("Area of Triangle:", triangleArea);
+
+//8. Store salary and calculate yearly salary
+
+let monthlySalary = 50000;
+let annualSalary = monthlySalary * 12;
+console.log("Annual Salary:", annualSalary);
+
+//9. Store hourly wage and daily income
+
+
+let  monthSalary = 50000;
+let day = monthSalary/ 31;
+let hour = day / 8;
+console.log("Salary per Hour:", hour);
+
+//10. Store price and quantity and calculate bill
+
+let pricePerItem = 150;
+let quantity = 4;
+let totalBill = pricePerItem * quantity;
+console.log("Total Bill:", totalBill);
+
+//11. Store monthly expense total
+
+let monthlIyExpense = 20000;
+let rent = 1000;
+let food = 200;
+let emi = 500;
+let monthlyExpenseTotal = rent + food + emi;
+console.log("Total Expense:", monthlyExpenseTotal);
+
+
