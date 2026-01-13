03a099f000ab3c959e12c1a39014caee5a1e142e
Abin vk <vettikkadanabin@gmail.com>
2026-01-13 19:43:41 +0530

    javascript

--- Javascript/Data Types/script.js
+++ Javascript/Data Types/script.js
@@ -20,3 +20,69 @@ console.log(c);
 
 let val = 2.4;
 console.log(val);
+
+//5. Check the type of a number using typeof.
+
+let num =12; // number
+let st = "abin"; // string
+let bo = true; // boolean
+
+console.log(typeof num);
+console.log(typeof st);
+console.log(typeof bo);
+
+//6. Store age as a number and print it.
+
+let age = 23;
+console.log(age);
+
+//7. Store salary as a number and print yearly salary.
+
+let salary = 3000;
+let year = salary*12;
+console.log(year);
+
+//8. Store length and width and calculate rectangle area.
+
+let length = 12;
+let width = 13;
+let area = length*width;
+console.log(area);
+
+//9. Store radius and calculate circle area.
+
+let radius = 7;
+let are = 3.14 * radius * radius;
+
+console.log(are);
+
+//10. Store marks and calculate total marks
+
+let English = 34;
+let Maths = 45;
+let totalMarks = (English+Maths);
+ 
+console.log(totalMarks);
+
+//11. Store marks and calculate average marks.
+
+let mat = 45;
+let eng = 54;
+let mal = 35;
+let tot =(mat+eng+mal);
+let ave = (tot/3);
+
+console.log(ave);
+
+//12. Store temperature in Celsius.
+
+let celsius = 30;
+
+console.log(celsius);
+
+//13. Convert Celsius to Fahrenheit.
+
+let celius = 30;
+let fahrenheit = (celius * 9 / 5) + 32;
+
+console.log(fahrenheit);
