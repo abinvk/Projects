cf3656e2b8726e5643a1e60e6d09501aa269572d
Abin vk <vettikkadanabin@gmail.com>
2026-01-13 19:15:49 +0530

    javascript

---
+++ Javascript/Data Types/script.js
@@ -0,0 +1,22 @@
+//1. Declare a variable and store a number.
+
+let a = 10;
+console.log(a);
+
+//2. Store two numbers and print their sum.2. Store two numbers and print their sum.
+
+let ab = 10;
+let ba = 10;
+
+let  sum = (ab + ba);
+console.log(sum);
+
+//3. Store a negative number and print it.
+
+let c = -2;
+console.log(c);
+
+//4. Store a decimal number and print it.
+
+let val = 2.4;
+console.log(val);
