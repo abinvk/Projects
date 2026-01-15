daf551c08dc97b408a503b63bc4ad75867d668fe
Abin vk <vettikkadanabin@gmail.com>
2026-01-15 18:03:09 +0530

    javascript

--- Javascript/Operators/script.js
+++ Javascript/Operators/script.js
@@ -9,3 +9,87 @@ let b = 21;
 let sum = a + b;
 
 console.log(sum);
+
+//2. Subtract two numbers using -
+
+let x = 12 - 10;
+console.log(x);
+
+let xy = 70;
+let yx = 20;
+let su = xy-yx;
+
+console.log(su);
+
+//3. Multiply two numbers using *
+
+let vy = 12*4;
+
+console.log(vy);
+
+let xi = 70;
+let ix = 20;
+let mu = xy*yx;
+
+console.log(mu);
+
+//4. Divide two numbers using /
+
+let vn = 12/4;
+
+console.log(vn);
+
+let xa = 70;
+let ax = 20;
+let ma = xy/yx;
+
+console.log(ma);
+
+//5. Find remainder using %
+
+let cx = 10 % 3;
+console.log(cx);
+
+let ca = 10;
+let cb = 3;
+
+let remainder = a % b;
+console.log(remainder); // 1
+
+//6. Add three numbers
+
+let xaa = 5;
+let xb = 10;
+let c = 15;
+
+let um = xaa + xb + c;
+console.log(um); // 30
+
+//7. Subtract a bigger number from a smaller number
+
+let dx = 20;
+let vx = 12;
+
+let sub = dx - vx;
+console.log(sub);
+
+//8. Multiply a number by 10
+
+let numx = 7;
+
+let result = numx * 10;
+console.log(result); // 70
+
+
+//9. Divide a number by 2
+
+let nume =  15
+
+let two = nume / 2 ;
+
+console.log(two);
+
+//10. Find remainder when 15 is divided by 4
+
+let remaindr = 15 % 4;
+console.log(remaindr); // 3
