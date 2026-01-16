e7ebd91901595a35052a78532b524e3f70710bdd
Abin vk <vettikkadanabin@gmail.com>
2026-01-16 19:46:17 +0530

    javascript

---
+++ Javascript/Variables Datatypes Operators/script.js
@@ -0,0 +1,62 @@
+//1. Declare a variable and store your name.
+
+let name = "abin";
+
+console.log(name);
+
+//2. Store age in a variable
+
+let age = 23;
+
+console.log(age);
+
+//3. Declare a variable without value
+
+let no ;
+
+console.log(no);
+
+//4. Store two numbers in variables
+
+let num1 = 1;
+let num2 = 2;
+
+console.log(num1);
+console.log(num2);
+
+//5. Store city name.
+
+let place = "malappuram";
+console.log(place);
+
+//6. Store country name.
+
+let contry = "UAE"
+
+console.log(contry);
+
+//7. Store phone number.
+
+let number = "9074";
+
+console.log(number);
+
+//8. Store email.
+
+let email = "abin@gamil.com123";
+
+console.log(email);
+
+//9. Store boolean value
+
+let bloo = true;
+
+console.log(bloo);
+
+//10. Update variable value
+
+let a = "b";
+
+a = "c";
+
+console.log(a);
