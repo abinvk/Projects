71e4c47c2fc4bcb2ce03561c6c7e8857a822d384
Abin vk <vettikkadanabin@gmail.com>
2026-01-22 10:01:48 +0530

    javascript

---
+++ Javascript/Conditions/script.js
@@ -0,0 +1,20 @@
+
+//1. Check if a number is positive
+
+let number = 10;
+
+if(number > 0){
+    console.log("number is positive");
+}else{
+    console.log("numer is negative");
+};
+
+// 2. Check if a number is negative
+
+let num = -5;
+
+if(num < 0 ){
+    console.log("number is negative");
+}else{
+    console.log("number is positive");
+};
