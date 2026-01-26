9b8b6326ed20c538e3cd378d127603b682ee7412
Abin vk <vettikkadanabin@gmail.com>
2026-01-26 21:25:22 +0530

    javascript

--- Javascript/Project 4 Salary to Yearly Salary Calculator/script.js
+++ Javascript/Project 4 Salary to Yearly Salary Calculator/script.js
@@ -1,6 +1,8 @@
 function calculateYearlySalary(){
+
     let monthly = Number(document.getElementById("monthlySalary").value);
-    let yearly = monthly*12;
+    let yearly = monthly * 12;
+
     document.getElementById("result").innerText =
     "Yearly Salary: ₹" + yearly;
 }
