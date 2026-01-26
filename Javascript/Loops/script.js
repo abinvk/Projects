2bc6f630a9cb9f5d24076d812744d8a4b4f0a566
Abin vk <vettikkadanabin@gmail.com>
2026-01-26 16:18:30 +0530

    javascript

--- Javascript/Loops/script.js
+++ Javascript/Loops/script.js
@@ -19,3 +19,41 @@ for(let s = 1; s <= 20; s++){
 }
 
 //4. Print even numbers from 1 to 50
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+//5. Print odd numbers from 1 to 50
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 === 1) {
+    console.log(i);
+  }
+}
+
+//6. Print numbers divisible by 5 (1–100)
+
+for (let i = 1; i <= 100; i++) {
+    if(i % 5 === 0){
+    console.log(i);
+    }
+  }
+
+//7. Print multiples of 3 (1–30)
+
+for (let i = 1; i <= 30; i++) {
+  if (i %  3 === 0) {
+    console.log(i);
+  }
+}
+
+//8. Print numbers from 0 to n
+
+let n = 20;
+for (let i = 1; i <= n; i++) {
+    console.log(i);
+  
+}
