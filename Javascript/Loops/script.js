5883caf8efce50c857cfa04c9986154bfb9c94c6
Abin vk <vettikkadanabin@gmail.com>
2026-01-26 15:52:43 +0530

    jaavscript

---
+++ Javascript/Loops/script.js
@@ -0,0 +1,21 @@
+// !  A. Basics – for / while / do…while
+ 
+//1. Print numbers from 1 to 10
+
+for(let i = 1; i <=10; i++){
+    console.log(i);
+}
+
+//2. Print numbers from 10 to 1
+
+for(let a = 10; a >=1; a--){
+    console.log(a);
+}
+
+//3. Print first 20 natural numbers
+
+for(let s = 1; s <= 20; s++){
+    console.log(s);
+}
+
+//4. Print even numbers from 1 to 50
