2e56fd31012bc0922e218a46fbb2958911a673a2
Abin vk <vettikkadanabin@gmail.com>
2026-01-27 11:45:42 +0530

    javascritpt

--- Javascript/Loops/script.js
+++ Javascript/Loops/script.js
@@ -227,3 +227,25 @@ for (let i = 50; i >= 1; i--) {
     console.log(i);
   }
 }
+
+// !  B. Sum / Count / Math Logic
+
+//31. Find sum of numbers from 1 to 10
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+  sum = sum + i;
+}
+
+console.log(sum);
+
+//32. Find sum of numbers from 1 to n
+
+let na = 10;
+let summ = 0;
+
+for (let i = 1; i <= na; i++) {
+  summ = summ + i;
+}
+console.log(summ);
