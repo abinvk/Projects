3b65ac42cc78e3417ecd7c31b49ff74a90361a97
Abin vk <vettikkadanabin@gmail.com>
2026-01-29 21:56:30 +0530

    javascript

---
+++ Study Projects/Project 6 Temperature Converter (C to F)/script.js
@@ -0,0 +1,12 @@
+function convertTemp(){
+    let celsius = Number(document.getElementById("celsius").value)
+   
+    if (isNaN(celsius)) {
+    document.getElementById("result").innerText = "Enter a valid number";
+    return;
+  }
+   let fahrenheit = (celsius * 9 / 5) + 32;
+
+     document.getElementById("result").innerText =
+    "Result: " + fahrenheit + " °F";
+}
