511cf84454123311420033cc5a0180425560b5b3
Abin vk <vettikkadanabin@gmail.com>
2026-01-30 21:18:32 +0530

    javascript

--- Study Projects/Project Overtime Salary Calculator/script.js
+++ Study Projects/Project Overtime Salary Calculator/script.js
@@ -1,15 +1,15 @@
 function total(){
-    let salary = (document.getElementById("salary").value)
-    let overtime = (document.getElementById("overtime").value)
+    let salary = Number(document.getElementById("salary").value)
+    let overtime = Number(document.getElementById("overtime").value)
 
     if (isNaN(salary) || isNaN(overtime)){
         document.getElementById("Result").innerText = 
         "Please enter valid numbers";
         return;
     }
-    
+
     let totals = salary + overtime;
 
     document.getElementById("Result").innerText =
-    "Total Salary: ₹" + totals
+    "Total Salary: ₹" + totals;
 }
