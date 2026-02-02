73794d9b9847224b9f37706b05c44978439a5a43
Abin vk <vettikkadanabin@gmail.com>
2026-02-02 20:00:14 +0530

    javascript

--- Study Projects/Project Type Checker Tool/script.js
+++ Study Projects/Project Type Checker Tool/script.js
@@ -1,4 +1,21 @@
 function checkType(){
     let value = document.getElementById("inputValue").value;
     let resultText = document.getElementById("result");
+
+    if (value === ""){
+        resultText.innerText = "Please enter a value";
+        return;
+    }
+
+    if (!isNaN(value)){
+        value = Number(value);
+    }
+
+    let type = typeof value;
+
+    if (value === null){
+        type = "null";
+    }
+
+    resultText.innerText = "Data Type: " + type;
 }
