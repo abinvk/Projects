937f5254e6e5a378ccbbfb47785aafdd931e669c
Abin vk <vettikkadanabin@gmail.com>
2026-02-13 19:51:07 +0530

    javascript

--- Javascript/Loops/script.js
+++ Javascript/Loops/script.js
@@ -2349,3 +2349,15 @@ for (let i = 1; i <= n; i++) {
 
   //console.log(row);
 }
+
+//157. Pattern using reverse loop
+
+let nxc = 5;
+
+for (let i = nxc; i >= 1; i--) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
