264511f7454871194f30c9ddcb31230cf9e0b3df
Abin VK <vettikkadanabin@gmail.com>
2026-02-17 18:56:45 +0530

    javascript

--- Javascript/Functionss/script.js
+++ Javascript/Functionss/script.js
@@ -6,3 +6,17 @@ function printHelloWorld() {
     console.log('Hello World');
 }
 printHelloWorld(); // Output: Hello World
+
+//2. Create a function that prints your name.
+
+function myName(){
+    console.log('My name is John');
+}
+myName(); // Output: My name is John
+
+// 3. Create a function that adds two numbers.\
+
+function addNumbers(a, b) {
+    console.log(a + b);
+}
+addNumbers(5, 10); // Output: 15
