409e2774b1f50f0d0bfa18d09270dc37c9366900
Abin VK <vettikkadanabin@gmail.com>
2026-02-17 19:02:25 +0530

    javascript

--- Javascript/Functionss/script.js
+++ Javascript/Functionss/script.js
@@ -14,9 +14,16 @@ function myName(){
 }
 myName(); // Output: My name is John
 
-// 3. Create a function that adds two numbers.\
+// 3. Create a function that adds two numbers.
 
 function addNumbers(a, b) {
     console.log(a + b);
 }
-addNumbers(5, 10); // Output: 15
+addNumbers(10, 15); // Output: 25
+
+// 4. Subtract two numbers.
+
+function addNumbers(a, b) {
+    console.log(a - b);
+}
+addNumbers(10, 5); // Output: 5
