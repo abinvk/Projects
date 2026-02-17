1faf7e49194168e82df0172ac7617b7bfb1a647d
Abin VK <vettikkadanabin@gmail.com>
2026-02-17 18:42:38 +0530

    javascript

---
+++ Javascript/Functionss/script.js
@@ -0,0 +1,8 @@
+// JavaScript Function – 200+ Practice Questions //
+
+//  1. Create a function that prints 'Hello World
+
+function printHelloWorld() {
+    console.log('Hello World');
+}
+printHelloWorld(); // Output: Hello World
