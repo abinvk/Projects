5e6d181200a3345054ca1df7be345881a35139ab
Abin VK <vettikkadanabin@gmail.com>
2026-02-19 21:23:02 +0530

    javascript

--- Javascript/Functionss/script.js
+++ Javascript/Functionss/script.js
@@ -832,3 +832,21 @@ function sortAscending(arr) {
 }
 console.log(sortAscending([5, 2, 9, 1]));
 
+//58. Sort descending manually.
+
+function sortdescending(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] < arr[j + 1]) {
+        // swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(sortdescending([5, 2, 9, 1]));
