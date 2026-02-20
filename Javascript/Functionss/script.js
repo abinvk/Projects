5a99fa08a79a0deaecdb983b9366eeb713c887e8
Abin VK <vettikkadanabin@gmail.com>
2026-02-20 12:05:52 +0530

    javascript

--- Javascript/Functionss/script.js
+++ Javascript/Functionss/script.js
@@ -879,3 +879,22 @@ for(let i = 0; i < arr.length; i++){
 return sum;
 }
 console.log(sumOfEven([1,2,3,4,5,6,7,8,9,]));
+
+// 61. Check if array is palindrome.
+
+function isPalindrome(arr){
+    let left = 0;
+    let right = arr.length -1;
+
+    while (left < right){
+        if (arr[left] !== arr[right]){
+            
+            return false;
+    }
+
+    left++;
+    right--;
+}
+return true;
+}
+console.log(isPalindrome([1,2,3,2,1]));
