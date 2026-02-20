3f3816a8a982f2e125703995857c4e726b4a7c82
Abin VK <vettikkadanabin@gmail.com>
2026-02-20 13:09:42 +0530

    javascript

--- Javascript/Functionss/script.js
+++ Javascript/Functionss/script.js
@@ -961,3 +961,23 @@ function revers(str){
     return reversed;
 }
 console.log(revers("Abin"))
+
+//66. Check palindrome string.
+
+function isPalindrome(str) {
+  
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+console.log(isPalindrome("madam"));
