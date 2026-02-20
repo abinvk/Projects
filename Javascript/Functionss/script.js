9aa3ce0e27451e4693a494786b160e614311a700
Abin VK <vettikkadanabin@gmail.com>
2026-02-20 14:21:16 +0530

    javascript

--- Javascript/Functionss/script.js
+++ Javascript/Functionss/script.js
@@ -1009,3 +1009,20 @@ function countWords(str) {
 }
 
 console.log(countWords("Hello world from JavaScript"));
+
+//69. Find longest word.
+
+function findLongestWord(str) {
+  let words = str.trim().split(/\s+/);
+  let longest = "";
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longest.length) {
+      longest = words[i];
+    }
+  }
+
+  return longest;
+}
+
+console.log(findLongestWord("JavaScript is very powerful"));
