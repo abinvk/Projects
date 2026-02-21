244532e4038b4f5605f7a68ad5e50568e40006df
Abin VK <vettikkadanabin@gmail.com>
2026-02-21 11:59:35 +0530

    javascript

--- Javascript/Functionss/script.js
+++ Javascript/Functionss/script.js
@@ -1463,3 +1463,22 @@ function findFactors(n) {
 }
 
 findFactors(12);
+
+//90. Count factors.
+
+function countFactors(n) {
+
+    let count = 0;
+
+    for (let i = 1; i <= n; i++) {
+
+        if (n % i === 0) {
+            count++;
+        }
+
+    }
+
+    console.log("Total Factors:", count);
+}
+
+countFactors(12);
