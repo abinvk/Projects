84add4d7a7888e930b617340a19c80d5dcbaea3a
Abin VK <vettikkadanabin@gmail.com>
2026-03-17 19:39:04 +0530

    javascript

--- Javascript/Arrays/script.js
+++ Javascript/Arrays/script.js
@@ -1893,3 +1893,17 @@ function isPalindrome(arr) {
     return true;
 }
 console.log(isPalindrome([1,2,3,2,1])); // true
+
+// 134. Find pair with given sum.
+
+function findPair(arr, target){
+    for (let i = 0; i < arr.length; i++){
+        for ( let j = i + 1; j < arr.length; i++){
+            if (arr[i] + arr[j] === target){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+    return null;
+}
+console.log(findPair([2,7,11,15], 9));
