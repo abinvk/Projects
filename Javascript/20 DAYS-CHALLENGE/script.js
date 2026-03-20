98193d8b8df6aa9cabe75410d3e2792af4e3b242
Abin VK <vettikkadanabin@gmail.com>
2026-03-20 08:05:39 +0530

    javascript

---
+++ Javascript/20 DAYS-CHALLENGE/script.js
@@ -0,0 +1,26 @@
+// PHASE 1 — Absolute Basics (2–3 days)
+
+// Goal: Remove syntax fear.
+
+// Day 1 — Variables
+
+let a = 10;
+let name = "Abin";
+
+console.log(a);
+console.log(name);
+
+// • Create 5 variables
+// • Print using console.log()
+
+let Place = "Malappuram";
+let HouseNo = "29-3";
+let Adress = "Vettikkadan";
+let Age = 23;
+let Area = "Melmuri";
+
+console.log(Place);
+console.log(HouseNo);
+console.log(Adress);
+console.log(Age);
+console.log(Area);
