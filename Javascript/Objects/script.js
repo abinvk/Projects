3bced6dafd607ccc97aab1d3f1964cba30ca5772
Abin VK <vettikkadanabin@gmail.com>
2026-03-28 20:39:57 +0530

    javascript

---
+++ Javascript/Objects/script.js
@@ -0,0 +1,10 @@
+                                       // JavaScript Objects – 200 Practice Questions //
+                                
+//     LEVEL 1: BASICS (1–40)    //
+
+// 1. Create an object with name and age
+
+let person = {
+    name: "Abin",
+    age: 21
+};
