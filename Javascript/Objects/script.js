3622b11486f39712ea254f2bc45e794a5ea248e3
Abin VK <vettikkadanabin@gmail.com>
2026-04-10 21:50:30 +0530

    javascript

--- Javascript/Objects/script.js
+++ Javascript/Objects/script.js
@@ -784,3 +784,40 @@ console.log(commonKeys);
 let missingKeys = Object.keys(objA2).filter(key => !(key in objB2));
 console.log(missingKeys);
 
+
+// 59. Compare two objects deeply
+
+
+function deepEqual(obj1, obj2) {
+    if (obj1 === obj2) return true;
+    if (typeof obj1 !== "object" || typeof obj2 !== "object" || obj1 === null || obj2 === null) {
+        return false;
+    }
+    let keys1 = Object.keys(obj1);
+    let keys2 = Object.keys(obj2);
+    if (keys1.length !== keys2.length) return false;
+    for (let key of keys1) {
+        if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+            return false;
+        }
+    }
+    return true;
+}
+let objC = {
+    name: "Abin",
+    age: 22,
+    address: {
+        city: "Alleppey",
+        state: "Kerala"
+    }
+};
+let objD = {
+    name: "Abin",
+    age: 22,
+    address: {
+        city: "Alleppey",
+        state: "Kerala"
+    }
+};
+console.log(deepEqual(objC, objD)); // true
+
